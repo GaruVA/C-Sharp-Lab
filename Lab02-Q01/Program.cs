ff8035309149075c99493195aae7ea7587b64140
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2024-02-07 22:38:53 +0530

    Lab2 Q1

---
+++ Lab02-Q01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_Q01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book();
+            myBook.title = "The Seven Moons of Maali Almeida";
+            myBook.author = "Shehan Karunatilaka";
+            Console.WriteLine($"Book Title: {myBook.title}");
+            Console.WriteLine($"Book Author: {myBook.author}");
+        }
+    }
+    class Book
+    {
+        public string title;
+        public string author;
+    }
+}
