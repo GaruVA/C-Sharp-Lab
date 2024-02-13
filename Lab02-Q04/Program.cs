a453f0fa11781498f9029b64d034bca5af323c3e
Garuka Veenath Assalaarachchi <garukavassalaarachchi@gmail.com>
2024-02-13 22:42:05 +0530

    Lab 2 - Q 4

---
+++ Lab02-Q04/Program.cs
@@ -0,0 +1,34 @@
+﻿// Construct a C# program for a basic inventory system.
+// Define a class named Product with properties ProductName and Price.
+// Implement a parameterized constructor to initialize these properties.
+// Instantiate objects using the constructor and display the product details.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_Q04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("Kar98k - Scoped", 1000000);
+            Console.WriteLine($"Product Name: {product.ProductName}");
+            Console.WriteLine($"Price: {product.Price}");
+        }
+    }
+    class Product
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public Product(string ProductName, decimal Price) 
+        { 
+            this.ProductName = ProductName;
+            this.Price = Price;
+        }
+    }
+}
